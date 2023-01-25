b3b35969f08422311b338937ee7456fe7e327143
manueldembo <madembo18@gmail.com>
2023-01-26 00:26:31 +0100

    @react-api:validação do capos de formulário-v1.0.0

--- reactjs/03-dt-money/src/components/SearchForm/index.tsx
+++ reactjs/03-dt-money/src/components/SearchForm/index.tsx
@@ -1,12 +1,35 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SeacrchFormContainer } from "./styles";
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SeacrchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm( ) {
-    return (
-        <SeacrchFormContainer>
-            <input placeholder="Buscar por transações" />
+    const {
+        register, 
+        handleSubmit,
+        formState: {isSubmitting}} = useForm<SeacrchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    })
 
-            <button type="submit">
+    function hadleSeachTransactions(data: SeacrchFormInputs) {
+
+    }
+
+    return (
+        <SeacrchFormContainer onSubmit={handleSubmit(hadleSeachTransactions)}>
+            <input 
+                placeholder="Buscar por transações" 
+                {...register('query')}
+            />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
                 Buscar
             </button>
--- reactjs/03-dt-money/src/components/SearchForm/styles.ts
+++ reactjs/03-dt-money/src/components/SearchForm/styles.ts
@@ -29,8 +29,14 @@ export const SeacrchFormContainer = styled.form`
         border: 1px solid ${props => props.theme["green-300"]};
         font-weight: bold;
         border-radius: 6px;
+        cursor: poiter;
 
-        &:hover {
+        &:disabled {
+            opacity: .6;
+            cursor: not-allowed;
+        }
+
+        &:not(:disabled):hover {
             background: ${props => props.theme["green-500"]};;
             color: ${props => props.theme.white};
             border-color: 1px solid ${props => props.theme["green-500"]};
